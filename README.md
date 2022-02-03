8c218832ff86523e019ed4e430566dee55db9262
shikishima <160442065@ccalumni.meijo-u.ac.jp>
2022-02-03 19:53:23 +0900

    Docker環境を追加

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # H5 KITTI-360
 
+The script to convert the [KITTI-360 dataset](http://www.cvlibs.net/datasets/kitti-360/index.php) to HDF5.
 
+## Preparation
+
+1. [Download the KITTI-360 dataset](http://www.cvlibs.net/datasets/kitti-360/user_login.php)
+1. Arrange the folders as shown in the [documentation](http://www.cvlibs.net/datasets/kitti-360/documentation.php).
+
+## Run on Docker (strongly recommended)
+
+1. Clone this repository with the following command.
+    ```bash
+    git clone https://github.com/shikishima-TasakiLab/h5_kitti360.git
+    ```
+
+1. Run the following command to perform the conversion.
+    ```bash
+    python docker/kitti2hdf5.py -d /path/to/KITTI-360 -o /path/to/output/dir -s SEQUENCE
+    ```
+
+## Run on Local Environment
+
+1. Install [pointsmap-python](https://github.com/shikishima-TasakiLab/pointsmap-python).
+
+1. Install [h5datacreator](https://github.com/shikishima-TasakiLab/h5datacreator).
+
+1. Install h5_kitti360 with the following command.
+    ```bash
+    pip install git+https://github.com/shikishima-TasakiLab/h5_kitti360
+    ```
+
+1. Run the following command to perform the conversion.
+    ```bash
+    kitti2hdf5 -d /path/to/KITTI-360 -o /path/to/output/dir -s SEQUENCE
+    ```
---
+++ docker/kitti2hdf5.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+import subprocess
+from typing import Dict
+import docker
+
+DOCKER_IMAGE = 'h5_kitti360'
+
+def build():
+    docker_dir: str = os.path.dirname(os.path.abspath(__file__))
+    print(docker_dir)
+
+    proc = subprocess.run([
+        'docker', 'build', '-t', DOCKER_IMAGE,
+        '--ulimit', 'stack=8192', docker_dir
+    ])
+    if proc.returncode != 0:
+        print(f'Failed: "{DOCKER_IMAGE}"')
+        return
+    print(f'Finished "{DOCKER_IMAGE}"')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset-root-dir', type=str, metavar='PATH', required=True, help='Root directory of KITTI-360 Dataset.')
+    parser.add_argument('-o', '--output-dir', type=str, metavar='PATH', required=True, help='Output Directory.')
+    parser.add_argument('-s', '--sequence', type=int, choices=[0,2,3,4,5,6,7,9,10], required=True, help='Sequence of KITTI-360 Dataset.')
+    args: Dict[str, str] = vars(parser.parse_args())
+
+    if os.path.isdir(args['dataset_root_dir']) is False:
+        raise ValueError(f'"{args["dataset_root_dir"]}" is not a directory.')
+    if os.path.isdir(args['output_dir']) is False:
+        raise ValueError(f'"{args["output_dir"]}" is not a directory.')
+
+    docker_client = docker.from_env()
+
+    try:
+        docker_client.images.get(DOCKER_IMAGE)
+    except docker.errors.ImageNotFound:
+        build()
+
+    container = docker_client.containers.run(
+        DOCKER_IMAGE,
+        command=f'kitti2hdf5 -d /workspace/KITTI-360 -o /workspace/HDF5 -s {args["sequence"]:d}',
+        remove=True,
+        stderr=True,
+        detach=True,
+        volumes={
+            os.path.abspath(args['dataset_root_dir']): {'bind': '/workspace/KITTI-360', 'mode': 'ro'},
+            os.path.abspath(args['output_dir']): {'bind': '/workspace/HDF5', 'mode': 'rw'}
+        }
+    )
+
+    container_output = container.attach(stdout=True, stream=True, logs=True)
+
+    for line in container_output:
+        print(line.decode('utf-8'))
+
+if __name__=='__main__':
+    main()
